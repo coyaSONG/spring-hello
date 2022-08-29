e96e6f16843be2bb3da159c3955587b9442c0ae2
coyaSONG <qawsy12@gmail.com>
2022-08-29 20:26:03 +0900

    회원 서비스 개발

---
+++ src/main/java/coya/hellospring/service/MemberService.java
@@ -0,0 +1,36 @@
+package coya.hellospring.service;
+
+import coya.hellospring.domain.Member;
+import coya.hellospring.repository.MemberRepository;
+import coya.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /** 회원가입 */
+    public Long join(Member member) {
+        //ex: 같은 이름을 가진 중복 회원 X
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName((member.getName()))
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
